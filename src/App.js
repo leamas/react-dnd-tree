4cb98332e60cc35c8a9c1d12cc92bbdba654b729
Alec Leamas <leamas.alec@gmail.com>
2016-12-04 09:25:43 +0100

    demo3 in place.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component } from 'react';
-import Demo1 from './demo1.js';
+import Demo3 from './demo3.js';
 
 export default  class App extends Component {
 
     render() {
         return (
             <div>
-                <h1> First demo: </h1>
-                <Demo1 />
+                <h1> Third demo: </h1>
+                <Demo3 />
             </div>
         );
     }
